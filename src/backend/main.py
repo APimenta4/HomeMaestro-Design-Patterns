2678ab4e583c78cc970e60a3b803d86342de3bf4
AFONSO PIMENTA <afonsocpimenta04@gmail.com>
2025-10-26 01:17:08 +0100

    chore: add more sample instances
    Signed-off-by: AFONSO PIMENTA <afonsocpimenta04@gmail.com>

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,22 +2,35 @@ from actions import ExternalAction
 from actions.commands import LightsCommand
 from devices import Device, DeviceStatus
 from devices.features import LampFeature
-from devices.hubs import ZWaveHub
+from devices.hubs import ZWaveHub, ZigbeeHub
 from home_maestro.home_maestro import HomeMaestro
 from integrations import TelegramIntegration, WhatsAppIntegration
 from rules import AutomationRule
 from triggers import TimeTrigger
 from triggers.conditions import LightsCondition
-from api import app
+from api.api import app
+
 
 if __name__ == "__main__":
 
     # Sample devices
     features = set()
-    features.add(LampFeature({"max_brightness": 100}))
-    device1 = Device(DeviceStatus.ONLINE, features)
-    device2 = Device(DeviceStatus.OFFLINE, features)
-    hub = ZWaveHub({device1})
+    features.add(LampFeature("My Dream Feature", {"max_brightness": 100}))
+    features.add(
+        LampFeature("Second Feature", {"second feature": "amazing capabilities"})
+    )
+    device1 = Device("Smartwatch", DeviceStatus.ONLINE, features)
+    device2 = Device("Digital Clock 2018", DeviceStatus.OFFLINE, features)
+    feature1 = LampFeature("Feature 1", {"something": "my test value"})
+    feature2 = LampFeature("Feature 2", {"something else": "20"})
+    feature3 = LampFeature("Feature 3", {"ADS feature": "10"})
+    device3 = Device(
+        "Test Device", DeviceStatus.OFFLINE, {feature1, feature2, feature3}
+    )
+    device4 = Device("Another Device", DeviceStatus.ERROR)
+    device5 = Device("Simple Device", DeviceStatus.ONLINE, {feature2})
+    hub = ZWaveHub("My first hub", DeviceStatus.ONLINE, {device1})
+    hub2 = ZigbeeHub("ZigBee Hub", DeviceStatus.ONLINE, {device1})
 
     # Sample integrations
     integration1 = WhatsAppIntegration()
@@ -31,10 +44,17 @@ if __name__ == "__main__":
     automation_rule1 = AutomationRule(trigger=trigger1, action=action1)
 
     home_maestro = HomeMaestro(
-        devices={hub, device2},
-        automation_rules={automation_rule1},
         integrations={integration1, integration2},
     )
+
+    home_maestro.add_device(hub)
+    home_maestro.add_device(hub2)
+    home_maestro.add_device(device2)
+    home_maestro.add_device(device3)
+    home_maestro.add_device(device4)
+    home_maestro.add_device(device5)
+
+    home_maestro.add_automation_rule(automation_rule1)
 
     # Start the API
     app.run(debug=True)
