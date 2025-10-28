b4e3f6509a54242703c0cb81f2b2217fa9a54592
Gustavo Oliveira <gustavo.oliveira@cerc.inf.br>
2025-10-28 15:10:51 +0000

    integrates frontend and backend for Feature #1 - List Devices

--- src/backend/api/api.py
+++ src/backend/api/api.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 from api.devices_api import devices_api
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(devices_api, url_prefix="/devices")
