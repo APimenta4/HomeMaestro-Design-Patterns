20dc652625896b879af352da07a7d91f0257695b
gust0717 <oliveiramartinsgustavo@gmail.com>
2025-12-06 20:44:33 +0000

    Implements the edition of features

--- src/backend/devices/features/temperature_feature.py
+++ src/backend/devices/features/temperature_feature.py
@@ -4,7 +4,7 @@ from . import Feature
 from typing import TypedDict
 
 class TemperatureParameters(TypedDict):
-    temperature: int
+    temperature: float
 
 
 class TemperatureFeature(Feature):
@@ -12,9 +12,12 @@ class TemperatureFeature(Feature):
         super().__init__(name, options)
         self.temperature = options.get("temperature", 0.0)
 
-    def execute(self):
-        # Virtual device, therefore we simulate temperature reading
-        self.temperature = round(random.uniform(15.0, 25.0))
+    def execute(self, options: TemperatureParameters | None = None):
+        if options and "temperature" in options:
+            self.temperature = float(options.get("temperature", self.temperature))
+            if self.options:
+                self.options["temperature"] = self.temperature
+        return f"Temperature set to {self.temperature}"
 
     def get_status(self):
         return {"temperature": self.temperature}
