d5ab7d3b02e15e901c784aa98aa093997dc9fa72
gust0717 <oliveiramartinsgustavo@gmail.com>
2025-12-07 22:32:11 +0000

    Implement command action

--- src/backend/automations/actions/commands/blinds_command.py
+++ src/backend/automations/actions/commands/blinds_command.py
@@ -2,9 +2,15 @@ from . import Command
 
 
 class BlindsCommand(Command):
-    def __init__(self):
-        # command related attributes
-        pass
+    def __init__(self, device_id: int, feature_id: int, options: dict[str, object] | None = None):
+        super().__init__(device_id, feature_id, options)
+        self.position: int | None = self.options.get("position")
 
     def execute(self):
         pass
+
+    def to_dict(self):
+        dict = super().to_dict()
+        dict["position"] = self.position
+
+        return dict
--- src/backend/automations/actions/commands/command_factory.py
+++ src/backend/automations/actions/commands/command_factory.py
@@ -5,15 +5,15 @@ from . import (Command, BlindsCommand, LampCommand, LockCommand,
 # DESIGN PATTERN: Simple Factory
 class CommandFactory:
     @staticmethod
-    def create_command(type: str) -> Command:
+    def create_command(type: str, device_id: int, feature_id: int, options: dict[str, object] | None = None) -> Command:
         match type.lower():
             case "lamp":
-                return LampCommand()
+                return LampCommand(device_id=device_id, feature_id=feature_id, options=options)
             case "lock":
-                return LockCommand()
+                return LockCommand(device_id=device_id, feature_id=feature_id, options=options)
             case "temperature":
-                return TemperatureCommand()
+                return TemperatureCommand(device_id=device_id, feature_id=feature_id, options=options)
             case "blinds":
-                return BlindsCommand()
+                return BlindsCommand(device_id=device_id, feature_id=feature_id, options=options)
             case _:
                 raise ValueError(f"The provided command type is not supported: {type}")
